803fe689a84c2f0d83f83951e5a0a9f8ccc37140
Will <william.aftring@outlook.com>
2021-04-28 13:23:15 -0400

    v1

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,10 @@
-#PCSV
+## DESCRIPTION:
+
+Parses CSV files for specific columns and prints the output to the console.
+
+**NOTE:** File must end in .csv
+
+## OPTIONS:
+
+- -t      Print the CSV headers
+- -i:     The index of the columns to be print
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <windows.h>
 
 #define HELP 0x1
 #define HEADER 0x2
@@ -13,20 +15,28 @@ char* g_Delimitter = ",";
 bool ParseArgs(int argc, char *argv[]);
 std::string ParseLine(const std::string* line, char* delimiter, int index);
 
-/* TODO
- * - Finish the arg parsing
- */
 int main(int argc, char *argv[])
 {
-
+    
+    size_t max_index = 0;
+    int result = 0;
     std::string line;
     std::ifstream csvfile("Test.csv");
 
+    if(argc < 2)
+    {
+        std::cout << "Not enough arguments. Use --help for more details." << std::endl;
+        result = 1;
+        goto Exit;
+    }
+
     if(!ParseArgs(argc, argv))
     {
-        std::cout << "Invalid parameters. --help for full options" << std::endl;
-        return 1;
+        std::cout << " --help for full options" << std::endl;
+        result = 1;
+        goto Exit;
     }
+
     if((g_Flags & DEBUG) == DEBUG)
     {
 
@@ -36,15 +46,55 @@ int main(int argc, char *argv[])
         std::cout << "g_Filename: " << g_Filename << std::endl;
 
     }
+
+    if((g_Flags & HELP) == HELP)
+    {
+        //TODO finish this
+        std::cout << "pcsv.exe [-t][][-i:#] CSVFile" << std::endl;
+        std::cout << "\nDESCRIPTION:" << std::endl;
+        std::cout << "\tParses CSV files for specific columns and prints the output to the console.NOTE: File must end in .csv" << std::endl;
+        std::cout << "\nOPTIONS:" << std::endl;
+        std::cout << "\t-t\tPrint the CSV headers" << std::endl;
+        std::cout << "\t-i:\tThe index of the columns to be print" << std::endl;
+        goto Exit;
+    }
+
+    //TODO add logic for max number of indecies
     if(csvfile.is_open())
     {
-        while(std::getline(csvfile, line))
+
+        std::getline(csvfile, line);
+        max_index = std::count_if(line.begin(), line.end(), [](char c) {
+                if(c == ',') 
+                { return true;}
+                return false;});
+
+        if(g_Index > max_index)
         {
-//            std::cout << ParseLine(&line, ",", 0) << std::endl;;
+            std::cout << "Invalid arguments. Index of " << g_Index << " is greater than column count of " << max_index << std::endl;
+            result = 2;
+            csvfile.close();
+            goto Exit;
+        }
+
+        if(g_Flags & HEADER)
+        {
+            std::cout << g_Filename << "header: " << std::endl;
+            std::cout << line << std::endl;
+        }
+        else
+        {
+            while(std::getline(csvfile, line))
+            {
+                std::cout << ParseLine(&line, ",", g_Index) << std::endl;;
+            }
         }
         csvfile.close();
     }
-    return 0;
+
+Exit:
+
+    return result;
 }
 
 std::string ParseLine(const std::string* line, char* delimiter, int index)
@@ -54,7 +104,6 @@ std::string ParseLine(const std::string* line, char* delimiter, int index)
     size_t initialPos = 0;
     int counter = 0;
     std::string result;
-
 
     while(pos != std::string::npos)
     {
@@ -76,26 +125,48 @@ std::string ParseLine(const std::string* line, char* delimiter, int index)
 bool ParseArgs(int argc, char* argv[])
 {
     bool ret = true;
-
+    DWORD dwFileInfo = 0;
     for(int i = 1; i < argc; i++)
     {
         std::string arg(argv[i]);
+
         if(arg.find("-i:") != std::string::npos)
         {
             size_t pos = arg.find(":");
-            g_Index = std::stoi(arg.substr(pos + 1), nullptr, 10); 
+            try 
+            {
+                g_Index = std::stoi(arg.substr(pos + 1), nullptr, 10); 
+            }
+            catch (...)
+            {
+                std::cout << "Invalid index"; 
+                ret = false;
+                goto Exit;
+            }
         }
-        else if(arg == "-t")
+
+        else if(arg == "-t" || arg == "--top")
             g_Flags = HEADER; 
         else if(arg == "--help")
+        {
             g_Flags = HELP;
+            goto Exit;
+        }
         else if(arg == "--debug")
             g_Flags |= DEBUG;
-        else
-            ret = false;
 
         arg.clear();
     }
+
+    g_Filename = argv[argc - 1];
+    dwFileInfo = GetFileAttributes(g_Filename.c_str()); 
+    if(dwFileInfo == INVALID_FILE_ATTRIBUTES || (dwFileInfo & FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY || g_Filename.substr(g_Filename.length() - 3) != ".csv")
+    {
+        std::cout << "Invalid file"; 
+        ret = false;
+    }
+
+Exit:
 
     return ret;
 }
